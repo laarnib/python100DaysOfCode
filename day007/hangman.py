b1c7e1c67c6410773caf166bc74ea2de7d2af6b0
laarnib <edulaarni@gmail.com>
2021-12-29 09:07:46 -0500

    added a loop to allow player to guess again until player has guessed all the letters of the chosen word.  When all letters are guessed, app tells player that they've won.

--- day007/hangman.py
+++ day007/hangman.py
@@ -12,24 +12,25 @@ else:
 
     #TODO-2-1: - Create an empty List called display.
     #For each letter in the chosen_word, add a "_" to 'display'.
-    #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] 
-    #with 5 "_" representing each letter to guess.
     display = []
     for letter in chosen_word:
         display.append("_")
 
     print(display)
-    #TODO-1-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-    guess = input("Guess a letter: ").lower()
-    print(guess)
-
+    print(display.count("_"))
+    #Ask the user to guess a letter until user has guessed all the letters by checking number of _s
+    while display.count("_") > 0:
+        guess = input("Guess a letter: ").lower()
+        print(guess)
+        
     #TODO-1-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-    #TODO-2-2 - If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-    #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].    
-    index = 0
-    for letter in chosen_word:
-        if guess == letter:
-            display[index] = letter
-        i += 1
+    #TODO-2-2 - If the letter at that position matches 'guess' then reveal that letter in the display at that position.  
+        index = 0
+        for letter in chosen_word:
+            if guess == letter:
+                display[index] = letter
+            index += 1
     
-    print(display)
+        print(display)
+    
+    print(f"The word is {chosen_word}. You win!")
