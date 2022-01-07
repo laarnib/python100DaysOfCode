78c7f11d1d3f309e05a55f01742cb781d2598c64
laarnib <edulaarni@gmail.com>
2022-01-07 13:46:19 -0500

    added padding of art from edge of the console screen modified data structure of items to include description and start bid price for each item stored welcome message in a variable added a function to animate welcome messsage refactored start_auction function added checking of invalid inputs in bid amount. Invalid inputs are not a number and bid amount is less than start bid price modified message displayed when auction is over

--- day009/project/blind_auction.py
+++ day009/project/blind_auction.py
@@ -1,18 +1,35 @@
 import os
 import time
+import sys
 
 #List of items for auction
 items = {
-    "camera": "Pentax  K1000 with a 50mm lens and cap.", 
-    "gameboy": "The original Nintendo Gameboy. It comes with Super Mario, Legend of Zelda, and Pokemon Red.", 
-    "wonderlamp": "Ever wanted to have your own genie? Well, here's your chance!\nRub the wonderlamp 150 times for Mr. Genie to appear.", 
-    "gramophone": "Need some vintage decoration in your home. Why not bring home this 1940 Gramophone", 
-    "bonsai": "The Chinese Elm Bonsai. It's a great starter tree for beginners."
+    "camera": {
+        "description": "Pentax  K1000 with a 50mm lens and cap.", 
+        "start_bid": 40
+    },
+    "gameboy": {
+        "description": "The original Nintendo Gameboy. It comes with Super Mario, Legend of Zelda, and Pokemon Red.", 
+        "start_bid": 45
+    },
+    "wonderlamp": {
+        "description": "Ever wanted to have your own genie? Well, here's your chance!\n\tRub the wonderlamp 150 times for Mr. Genie to appear.", 
+        "start_bid": 150
+    },
+    "gramophone": {
+        "description": "Need some vintage decoration in your home. Why not bring home this 1940 Gramophone", 
+        "start_bid": 350
+    },
+    "bonsai": {
+        "description": "The Chinese Elm Bonsai. It's a great starter tree for beginners.",
+        "start_bid": 50
     }
+}
 
 #Bidders' data
 bidders = {}
 
+welcome_msg = "Welcome to the secret auction program"
 
 #Prints an ASCII art saved in a txt file
 #The value passed must match the name of the file where
@@ -22,30 +39,57 @@ def print_art(item):
     item_art = open(filename, 'r')
     print(''.join([line for line in item_art]))
 
+def print_welcome_msg():
+    print("\n\t", end="")
+    for letter in welcome_msg:
+        print(f"{letter} ", end="")
+        sys.stdout.flush() 
+        time.sleep(0.15)
+
 #Clears the console
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def start_auction():
+def start_auction(item, description, start_price):
     #Auction keeps going until user says there are no more bidders.
     #Auction also stops if user enters a string other than 'yes' or 'no'.
-    while True:
-        bidder_name = input("What is your name? ")
-        bid_amount = int(input("What is your bid? "))
 
-        bidders[bidder_name] = bid_amount
+    continue_auction = "yes"
+    while continue_auction == "yes":
+        clear_console()
+        print("\n\n\n")
+        print_art(item)
+        print(f"\t{description}")
+        print(f"\tThe starting bid price is ${start_price}")
+        print("\n\n")
+        bidder_name = input("\tWhat is your name? ")
+        bid_amount = input("\tWhat is your bid? ")
 
-        continue_auction = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
-
-        if continue_auction == 'yes':
-            clear_console()
-            continue;
-        elif continue_auction == 'no':
-            break;
+        if not bid_amount:
+           print("\tBid amount is not a valid number. Bidder ignored.")
+           time.sleep(1)
+           continue
         else:
-            print(f"You entered an invalid response {continue_auction}. Ending auction...")
+            bid_amount = int(bid_amount)
+            if not bidder_name:
+                print("\tInvalid name. Bidder ignored.")
+                time.sleep(1)
+                continue
+            elif bid_amount < start_price:
+                print("\tBid price is less than starting bid price. Bidder ignored.")
+                time.sleep(1)
+                continue
+            else:
+                bid_amount = int(bid_amount)
+                bidders[bidder_name] = bid_amount
 
+        continue_auction = input("\tAre there any other bidders? Type 'yes' or 'no': ").lower()
+        if continue_auction == 'yes':
+            continue
+        else:
+            print(f"\tYou entered {continue_auction}. Ending the bid.")
+    
 
 def get_winner():
     winner = ""
@@ -58,21 +102,25 @@ def get_winner():
     return winner
 
 
+
 #Prints Welcome message and logo
 clear_console()
+print("\n\n\n")
 print_art("logo")
-print("Welcome to the secret auction program.\n\n")
+print_welcome_msg()
+print("\n\n")
 
-print("The first item is a...\n")
-time.sleep(3)
+print("\tThe first item is a...\n")
+time.sleep(2)
 for item in items:
-    clear_console()
-    print_art(item)
-    print(f"\n{items[item]}")
-    print("\n")
-    start_auction()
+    item_description = items[item]["description"]
+    item_start_price = items[item]["start_bid"]
+    start_auction(item, item_description, item_start_price)
     winner = get_winner()
-    print(f"The winning bid is {winner} with a bid of ${bidders[winner]}.")
-    time.sleep(3)
+    print(f"\n\tThe winning bid is {winner} with a bid of ${bidders[winner]}.")
+    print("\tThe next item is...")
+    time.sleep(2)
 
-print("Thank you for coming to the auction! Congratulations to all the winning bids!")
+clear_console()
+print("\n\n\tThat's all the items we have today.")
+print("\tThank you for coming to the auction! Congratulations to all the winning bids!\n\n")
