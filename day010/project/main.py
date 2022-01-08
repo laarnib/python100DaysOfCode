fe3d23c7a879a71f7070e1e1b73849701d0ce810
laarnib <edulaarni@gmail.com>
2022-01-07 19:59:30 -0500

    fixed calculator spelling

--- day010/project/main.py
+++ day010/project/main.py
@@ -3,7 +3,7 @@ import helper
 
 helper.clear_console()
 print("\n\n\tWelcome to the Calculator App\n\n")
-user_response = input("\tPress any key to start the calculor, or type 'x' to exit: ").lower()
+user_response = input("\tPress any key to start the calculator, or type 'x' to exit: ").lower()
 if user_response == 'x':
     exit()
 else:
